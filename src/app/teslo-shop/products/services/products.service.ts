ceceb0788cf62cd6c4960a647a2ab7f387416d4d
JMHadga <jose.galdon12@gmail.com>
2025-04-15 19:14:38 +0200

    Pantalla de producto, carousel de imágenes y productos por género creadas

--- src/app/teslo-shop/products/services/products.service.ts
+++ src/app/teslo-shop/products/services/products.service.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ProductsResponse } from '@products/interfaces/product.interface';
+import { Options } from '@products/interfaces/options.interface';
+import {
+  Product,
+  ProductsResponse,
+} from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +16,20 @@ import { Observable, tap } from 'rxjs';
 export class ProductsService {
   #http = inject(HttpClient);
 
-  getProducts(): Observable<ProductsResponse> {
+  getProducts(options: Options): Observable<ProductsResponse> {
+    const { limit = 9, offset = 0, gender = '' } = options;
     return this.#http
-      .get<ProductsResponse>('http://localhost:3000/api/products')
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit: limit,
+          offset: offset,
+          gender: gender,
+        },
+      })
       .pipe(tap((resp) => console.log(resp)));
+  }
+
+  getProductByIdOrSlug(idSlug: string): Observable<Product> {
+    return this.#http.get<Product>(`${baseUrl}/products/${idSlug}`);
   }
 }
